1e6343e85e368d4f8ac5d94ec35ec34fd2b4916a
Yassminelbialy <Yassminelbialy@gmail.com>
2020-03-30 01:09:01 +0200

    validate email

--- Custom/hms/model/hms_data_base.py
+++ Custom/hms/model/hms_data_base.py
@@ -1,7 +1,9 @@
 from odoo import models, fields, api
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from odoo.exceptions import ValidationError
 
+import re
 
 class Patient(models.Model):
     _rec_name = "first_name"
@@ -12,6 +14,7 @@ class Patient(models.Model):
     first_name = fields.Char()
     birth_date = fields.Date()
     age = fields.Integer(compute="compute_age")
+    email=fields.Char();
     last_name = fields.Char()
     cr_ratio = fields.Float()
     address = fields.Char()
@@ -28,14 +31,14 @@ class Patient(models.Model):
     department_id = fields.Many2one("hms.department")
     doctor_id = fields.Many2many("hms.doctor")
     log_ids = fields.One2many("hms.patient_log", "patient_id")
-
+    # Computer age from birth day
     @api.multi
     @api.depends('birth_date')
     def compute_age(self):
          for record in self:
               age = relativedelta(datetime.now().date(), fields.Datetime.from_string(record.birth_date)).years
               record.age = age
-
+    # Change patient state
     def change_state(self):
         if self.state == "Undetermined":
             self.state = "Good"
@@ -52,7 +55,13 @@ class Patient(models.Model):
             self.pcr = True
             return {"warning": {"title": "PCR", "message": "the pcr is now checked"}}
 
+    @api.constrains('email')
+    def validate_email(self):
+         for obj in self:
+               if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", obj.email) == None:
+                   raise ValidationError("Please Provide valid Email Address: %s" % obj.email)
 
+    _sql_constraints = [('dulicated email','UNIQUE (email)','email all already exists') ]
 class PatientLog(models.Model):
     _name = "hms.patient_log"
 
